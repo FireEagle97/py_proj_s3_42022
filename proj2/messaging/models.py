33d57d10a882d24763d47c57d9d5f7162c6c7f22
Michael Obadia <michael.obadia@dawsoncollege.qc.ca>
2022-04-28 16:09:42 -0400

    Added models for messaging part

--- proj2/messaging/models.py
+++ proj2/messaging/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+  msg_text = models.CharField(max_length=200, help_text="Enter your message")
+  send_date = models.DateTimeField("Sent on")
+  # potentially use dt.now for dates upon instantiation
+  sender = models.CharField(max_length=100)
+  recipient = models.CharField(max_length=100)
+  unread = models.BooleanField(default=False)
+  
+
+class Comment(models.Model):
+  comment_text = models.CharField(max_length=500)
+  sender = models.CharField(max_length=100)
+  send_date = models.DateTimeField("Sent on")
+
+class Notif(models.Model):
+  amount = models.IntegerField(default=0)
