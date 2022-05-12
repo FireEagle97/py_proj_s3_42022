165d4cff205e25960e090860d04de2f48d967079
minh duc hoang <2003minhduchoang@gmail.com>
2022-05-12 12:57:30 -0400

    pushing to main

--- proj2/user_management/forms.py
+++ proj2/user_management/forms.py
@@ -10,11 +10,11 @@ class UserRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2'
+        fields = ['username', 'email', 'password1', 'password2',
                   'last_name', 'first_name']
 
 
-from ..administration.models import Member
+from administration.models import Member
 
 
 class MemberForm(forms.ModelForm):
--- proj2/user_management/views.py
+++ proj2/user_management/views.py
@@ -10,7 +10,7 @@ from user_management.forms import UserRegistrationForm, MemberForm, UserEditForm
 # Create your views here.
 
 from .forms import UserRegistrationForm
-from ..administration.models import Member
+from administration.models import Member
 
 
 class RegisterView(CreateView):
@@ -81,66 +81,3 @@ class EditView(UpdateView):
             else:
                 messages.error(request, "Error occurred")
         return render(request, self.template_name, {'edit_form': edit_form, 'member_form': member_form})
-# def home(req):
-#     template_name='user_management/home.html'
-#     print (f"req.user.username {req.user.username}")
-#     amember = Member.objects.get(user__username = req.user.username)
-#     context = {'amember': amember}
-#
-#     return render (req, template_name, context)
-
-# def register(request):
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             username = form.cleaned_data.get('username')
-#             password = form.cleaned_data.get('password1')
-#             user = authenticate(username = username, password = password)
-#             login(request, user)
-#             return redirect('home')
-#     else:
-#         form = UserCreationForm()
-#     return render(request, 'registration/register.html', {'form': form})
-
-
-# def register(request):
-#     if request.method == "POST":
-#         print(f"type post {type(request.POST)}")
-#         reg_form = UserRegistrationForm(request.POST)
-#         if reg_form.is_valid():
-#             username = reg_form.cleaned_data.get('username')
-#             reg_form.save()  # save user data (username, pass, email., ...
-#             return redirect('member_login')
-#         else:
-#             messages.error(request, f"some error while creating account")
-#     else:
-#         reg_form = UserRegistrationForm()
-#     return render(request, 'user_management/register.html', {'reg_form': reg_form})
-
-# def register(request):
-#     template_name = 'member/register.html'
-#     if request.method == "POST":
-#         print(f"type post {type(request.POST)}")
-#         reg_form = UserRegistrationForm(request.POST)
-#
-#         if reg_form.is_valid():
-#             username = reg_form.cleaned_data.get('username')
-#             reg_form.save()  # save user data (username, pass, email., ...
-#             if not member_form.is_valid():
-#                 raise ValueError(f" error with pic uplaod ...")
-#             else:
-#                 affiliation = member_form.cleaned_data.get('affiliation')
-#                 user_pic = member_form.cleaned_data.get('user_pic')
-#                 the_user = User.objects.get(username=username)
-#                 Member.objects.create(affiliation=affiliation, user=the_user,
-#                                       user_pic=user_pic)
-#                 messages.success(request, f"Accout successfuly created fro {username}")
-#                 return redirect('member_login')
-#         else:
-#             messages.error(request, f"some error while creating account")
-#     else:
-#         reg_form = UserRegistrationForm()
-#         member_form = MemberForm()
-#
-#     return render(request, template_name, {'reg_form': reg_form, 'member_form': member_form})
