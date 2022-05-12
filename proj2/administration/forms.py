0da2a7d7352356f54f0f57e29ab3da4cf36fd65a
chris-dage <chris.j.dage@gmail.com>
2022-05-12 10:42:01 -0400

    added delete functionality

--- proj2/administration/forms.py
+++ proj2/administration/forms.py
@@ -2,16 +2,30 @@ from django import forms
 from django.forms import ModelForm
 from .models import Member
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.forms import UserChangeForm
 
 # Create an Item form
+# class MemberForm(ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ('first_name', 'last_name', 'email')
+#
+#         widgets = {
+#             'first_name': forms.TextInput(attrs={'class':'form-control'}),
+#             'last_name': forms.TextInput(attrs={'class':'form-control'},),
+#             'email': forms.TextInput(attrs={'class':'form-control'}),
+#         }
+
 class MemberForm(ModelForm):
     class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'username', 'email')
+        model = Member
+        fields = ['avatar']
 
-        widgets = {
-            'first_name': forms.TextInput(attrs={'class':'form-control'}),
-            'last_name': forms.TextInput(attrs={'class':'form-control'},),
-            'username': forms.TextInput(attrs={'class':'form-control'}),
-            'email': forms.TextInput(attrs={'class':'form-control'}),
-        }
+class UserEditForm (UserChangeForm):
+    email = forms.EmailField(required=True)
+    last_name = forms.CharField(max_length="50", required=False)
+    first_name = forms.CharField(max_length="50", required=False)
+
+    class Meta:
+        model=User
+        fields = ('email', 'last_name', 'first_name')
--- proj2/administration/views.py
+++ proj2/administration/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.views.generic import ListView, DetailView, FormView
 from .models import Member
-from .forms import MemberForm
+from .forms import MemberForm, UserEditForm
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User, Group
+
 
 # Create your views here.
 def home(req):
@@ -20,27 +26,70 @@ class MyListMemberView(View):
         }
         return render(request, self.template_name, context=data)
 
-class MyEditMemberDetailView(FormView):
+# class MyEditMemberDetailView(FormView):
+#     model = Member
+#     template_name = 'administration/user_edit.html'
+#     form_class = MemberForm
+#
+#     def get(self, request, *args, **kwargs):
+#         member = get_object_or_404(Member, id=kwargs['pk'])
+#
+#         return render(request, self.template_name, {'member': member})
+
+class CreateUserClassView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     model = Member
-    template_name = 'administration/user_edit.html'
     form_class = MemberForm
+    success_url = reverse_lazy('administration_user_list')
+    success_message = "user was created successfully ..."
+    extra_context = {
+        'form_legend': 'Create a New User',
+        'form_submit_btn': "Create User"
+    }
+
+    def form_valid(self, form):
+        form.instance.owner = \
+            Member.objects.get(user=self.request.user)
+        return super().form_valid(form)
+
+
+class UpdateUserClassView(UserPassesTestMixin, SuccessMessageMixin, UpdateView):
+    model = Member
+    form_class = UserEditForm
+    success_url = reverse_lazy('administration_user_list')
+    success_message = "user was updated successfully ..."
+    extra_context = {
+        'form_legend': 'Update a user',
+        'form_submit_btn': "Update"
+    }
+
+    def test_func(self):
+        userUpdate = self.get_object()
+
+        # print(f"res:{self.request.user == userUpdate}")
+        # return self.request.user == userUpdate
+
+        print(f"res:{1 == 1}")
+        return 1 == 1
+
+
+class DeleteUserClassView(UserPassesTestMixin, SuccessMessageMixin, DeleteView):
+    model = Member
+    success_url = reverse_lazy('administration_user_list')
+    success_message = "user was successfully deleted ..."
+
+    def test_func(self):
+        user_delete = self.get_object()
+
+        print(f"res:{1 == 1}")
+        return 1 == 1
+
+
+class GroupUserClassView(View):
+    template_name = 'administration/user_group.html'
 
     def get(self, request, *args, **kwargs):
-        member = get_object_or_404(Member, id=kwargs['pk'])
-
-        return render(request, self.template_name, {'member': member})
-
-# class UpdateUserView(LoginRequiredMixin, SuccessMessageMixin,
-#                           UserPassesTestMixin, UpdateView):
-#     model = Member
-#     fields = ('title', 'body')
-#     success_url = reverse_lazy('weblog_list_all')
-#     success_message = "You justr updated a post!"
-#     extra_context = {
-#         'for_toitl': "Update USING CBV",
-#         'btn_val': "Update PPPOST"
-#     }
-#
-#     def test_func(self):
-#         current_user = self.get_object()
-#         return self.request.user == current_user.user
+        res = Member.objects.all()
+        data = {
+            'usr_list': res
+        }
+        return render(request, self.template_name, context=data)
