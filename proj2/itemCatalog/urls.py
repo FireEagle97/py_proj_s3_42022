8e1b622ff7847cad89b5035ab8b7e6d722944305
dany <dany.makhoul@dawsoncollege.qc.ca>
2022-05-12 21:31:47 -0400

    added comment and rate

--- proj2/itemCatalog/urls.py
+++ proj2/itemCatalog/urls.py
@@ -4,9 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.MyListItemsView.as_view(), name='home'),
     path('home/', views.MyListItemsView.as_view(), name='home'),
-    path('item/<int:pk>', views.MyItemDetail.as_view(), name='itemCatalog_item_detail'),
+    path('item/<int:pk>', views.ItemReviewDetail.as_view(), name='itemCatalog_item_detail'),
     path('item/create/', views.CreateItemClassView.as_view(), name="item-create"),
-    path('item/update/<int:pk>',views.UpdateItemClassView.as_view(), name='item-update'),
-    path('item/delete/<int:pk>',views.DeletePostClassView.as_view(),name='item-delete'),
+    path('item/update/<int:pk>', views.UpdateItemClassView.as_view(), name='item-update'),
+    path('item/delete/<int:pk>', views.DeletePostClassView.as_view(), name='item-delete'),
+    # path('item/<int:pk>', views.ItemReviewDetail.as_view(), name='item-detain'),
 
 ]
--- proj2/itemCatalog/views.py
+++ proj2/itemCatalog/views.py
@@ -1,11 +1,14 @@
 # Create your views here.
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import View
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormMixin
+
 from administration.models import Member
-from .models import Item
-from .forms import ItemForm
+from .models import Item, Review
+from .forms import ItemForm, ReviewForm
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
@@ -27,17 +30,60 @@ class MyItemDetail(DetailView):
     model = Item
 
 
+class ItemReviewDetail(FormMixin, DetailView):
+    template_name = 'itemCatalog/item_detail.html'
+    model = Item
+    form_class = ReviewForm
+
+    def get(self, request, *args, **kwargs):
+        the_item = get_object_or_404(Item, id=kwargs.get('pk'))
+        review_form = ReviewForm
+        item_reviews = Review.objects.filter(item_id = the_item.id)
+
+        context = {
+            'item': the_item,
+            'review_form': review_form,
+            'item_reviews': item_reviews,
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        # if not request.user.is_authenticated:
+        #     return HttpResponseForbidden()
+        the_item = get_object_or_404(Item, id=kwargs.get('pk'))
+        review_form = ReviewForm(request.POST)
+        context = {
+            'item': the_item,
+            'review_form': review_form,
+
+        }
+        print("printing form")
+        # self.object = self.get_object()
+
+        if review_form.is_valid():
+            print("inside form")
+            member = Member.objects.get(user=request.user)
+            Review.objects.filter(item=the_item).filter(author=member).delete()
+            Review.objects.create(rate=review_form.instance.rate, item=the_item, author=member,
+                                  comment=review_form.instance.comment)
+            messages.success(request, 'Successfullly added a review')
+            return render(request, self.template_name, context)
+        else:
+            messages.error(request, f'an error has occured')
+            return render(request, self.template_name, context)
+
+
 class MyListItems(ListView):
     model = Item
 
 
-class MyItemDetailView(View):
-    model = Item
-    template_name = 'itemCatalog/item_detail.html'
-
-    def get(self, req, *args, **kwargs):
-        my_item = get_object_or_404(Item, id=kwargs['id'])
-        return render(req, self.template_name, {'i': my_item})
+# class MyItemDetailView(View):
+#     model = Item
+#     template_name = 'itemCatalog/item_detail.html'
+#
+#     def get(self, req, **kwargs):
+#         my_item = get_object_or_404(Item, id=kwargs['id'])
+#         return render(req, self.template_name, {'i': my_item})
 
 
 class CreateItemClassView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
@@ -67,10 +113,10 @@ class UpdateItemClassView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessag
     }
 
     def test_func(self):
-        post2update = self.get_object()
+        item2update = self.get_object()
 
-        print(f"res:{self.request.user == post2update.owner.user}")
-        return self.request.user == post2update.owner.user
+        print(f"res:{self.request.user == item2update.owner.user}")
+        return self.request.user == item2update.owner.user
 
 
 class DeletePostClassView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, DeleteView):
