3f9955a4fb11e4b64581e70b066f3ccac5ba690d
chris-dage <chris.j.dage@gmail.com>
2022-05-15 14:59:39 -0400

    chnages to group

--- proj2/administration/models.py
+++ proj2/administration/models.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.models import User, Group
 from django.db import models
 
-
+group_choices = ((1, "Members"), (2, "User Admin"), (3, "Item Admin"), (4, "Super User"))
 # Create your models here.
 
 class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, choices=group_choices ,on_delete=models.CASCADE)
     is_flagged = models.BooleanField(default=False)
     avatar = models.ImageField(upload_to='user_pics',
                               default='user_default_image.png')
-
 
 class Member_Warn(models.Model):
     member = models.OneToOneField(Member, on_delete=models.CASCADE)
