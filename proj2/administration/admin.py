236765aeab5516a52d60641f9d747b6219602465
dany <dany.makhoul@dawsoncollege.qc.ca>
2022-05-15 14:44:51 -0400

    pulling from merge_dany after testing it

--- proj2/administration/admin.py
+++ proj2/administration/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from administration.models import *
 
-all_models = (Member)
+all_models = (Member, Member_Warn)
 admin.site.register(all_models)
 # Register your models here.
