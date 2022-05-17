fd2bff23ea0d91aac7acae6d84807cb6a2dbd2bb
chris-dage <chris.j.dage@gmail.com>
2022-05-16 21:24:47 -0400

    slight modifications to image

--- proj2/administration/models.py
+++ proj2/administration/models.py
@@ -13,14 +13,3 @@ class Member(models.Model):
     is_warned = models.BooleanField(default=False)
     avatar = models.ImageField(upload_to='user_pics/',
                                default='user_default_image.png')
-
-    # def save(self):
-    #     super().save()
-    #
-    #     img = Image.open(self.image.path)  # Open image
-    #
-    #     # resize image
-    #     if img.height > 300 or img.width > 300:
-    #         output_size = (300, 300)
-    #         img.thumbnail(output_size)  # Resize image
-    #         img.save(self.image.path)  # Save it again and override the larger image
